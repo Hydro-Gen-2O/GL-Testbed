eee0191b2b2b1fd71f95dfd4dc57e955d212f406
esiaero <anthias@seas.upenn.edu>
2022-03-15 17:46:33 -0400

    boilerplate triangle

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <fstream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+GLuint create(const char* f, const char* g);
 
 // window size
 const unsigned int SCR_WIDTH = 800;
@@ -38,6 +40,25 @@ int main()
         return -1;
     }
 
+    GLuint prog = create("../glsl/test.vert.glsl", "../glsl/test.frag.glsl");
+
+    // set up triangle vbo vao
+    float vertices[] = {
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+         0.0f,  0.5f, 0.0f   // top 
+    };
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -45,6 +66,9 @@ int main()
         // input
         // -----
         processInput(window);
+
+        glUseProgram(prog);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -56,6 +80,58 @@ int main()
     // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
+}
+
+// BOILERPLATE adapted FROM HERE: 
+// https://github.com/JoeyDeVries/LearnOpenGL/blob/master/src/1.getting_started/3.1.shaders_uniform/shaders_uniform.cpp
+GLuint compileShader(std::string path, GLenum shaderType) {
+    // grab the contents of the file and store the source code in a string
+    std::ifstream fs(path, std::ios::in);
+
+    if (!fs.is_open()) {
+        std::cerr << path << " does not exist?" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    std::string shaderSource((std::istreambuf_iterator<char>(fs)), std::istreambuf_iterator<char>());
+    
+    GLuint shader = glCreateShader(shaderType);
+    const char* shaderSourcePtr = shaderSource.c_str();
+    glShaderSource(shader, 1, &shaderSourcePtr, NULL);
+    glCompileShader(shader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    return shader;
+}
+
+GLuint create(const char* vertFile, const char* fragFile) {
+    GLuint vertShader = compileShader(vertFile, GL_VERTEX_SHADER);
+    GLuint fragShader = compileShader(fragFile, GL_FRAGMENT_SHADER);
+
+    GLuint prog;
+    prog = glCreateProgram();
+    glAttachShader(prog, vertShader);
+    glAttachShader(prog, fragShader);
+    glLinkProgram(prog);
+
+    // check for linking errors
+    GLint success;
+    glGetProgramiv(prog, GL_LINK_STATUS, &success);
+    char infoLog[512];
+    if (!success) {
+        glGetProgramInfoLog(prog, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertShader);
+    glDeleteShader(fragShader);
+    return prog;
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
